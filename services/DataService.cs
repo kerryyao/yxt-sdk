921d4f56e6912af0c9ccc3fcc0937892935d2978
suguo.yao <yaokerry@gmail.com>
2017-03-06 20:01:29 +0800

    init

---
+++ services/DataService.cs
@@ -0,0 +1,11 @@
+namespace com.school114.services
+{
+    /// <summary>
+    /// suguo.yao 2017-3-6
+    /// 冀校通基础数据接口
+    /// </summary>
+    public static class DataService
+    {
+
+    }
+}
---
+++ services/MessageService.cs
@@ -0,0 +1,11 @@
+namespace com.school114.services
+{
+    /// <summary>
+    /// suguo.yao 2017-3-6
+    /// 冀校通短信通道
+    /// </summary>
+    public static class MessageService
+    {
+
+    }
+}
